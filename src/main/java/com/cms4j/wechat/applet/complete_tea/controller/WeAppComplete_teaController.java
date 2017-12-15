a678b8e533ed25fbae1939e39a56c56fc880cecd
ls <xiaoheima121@163.com>
2017-12-15 13:26:15 +0800

    ls:修改院校咨询返回结果集

--- src/main/java/com/cms4j/wechat/applet/complete_tea/controller/WeAppComplete_teaController.java
+++ src/main/java/com/cms4j/wechat/applet/complete_tea/controller/WeAppComplete_teaController.java
@@ -2,6 +2,7 @@ package com.cms4j.wechat.applet.complete_tea.controller;
 
 import com.cms4j.base.controller.ApiBaseController;
 import com.cms4j.base.util.DataMap;
+import com.cms4j.base.util.InvokeResult;
 import com.cms4j.plant.user.service.CompleteTeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,11 +20,11 @@ public class WeAppComplete_teaController extends ApiBaseController {
     @Autowired
     private CompleteTeacherService completeTeacherService;
 
-    @RequestMapping(value = "/get" ,method = RequestMethod.GET)
-    public List<DataMap> getCompletTeacher()throws  Exception {
+    @RequestMapping(value = "/get" ,method = RequestMethod.POST)
+    public InvokeResult getCompletTeacher()throws  Exception {
         DataMap dataMap= this.getDataMap();
         List<DataMap> list = completeTeacherService.getCompleteTeacherByScId(dataMap);
 
-        return list;
+        return InvokeResult.success(list);
     }
 }

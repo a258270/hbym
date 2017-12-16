770f3a77f1f157e565584cfbf13cfb7b194cb708
zmj <a258270@qq.com>
2017-12-16 18:54:34 +0800

    增加微信小程序根据院校和专业获取分数

--- src/main/java/com/cms4j/wechat/applet/major/controller/WechatAppMajorController.java
+++ src/main/java/com/cms4j/wechat/applet/major/controller/WechatAppMajorController.java
@@ -3,7 +3,10 @@ package com.cms4j.wechat.applet.major.controller;
 import com.cms4j.base.controller.ApiBaseController;
 import com.cms4j.base.util.DataMap;
 import com.cms4j.base.util.InvokeResult;
+import com.cms4j.base.util.SessionUtil;
+import com.cms4j.plant.school.mjscore.service.MjscoreService;
 import com.cms4j.plant.school.service.HasmajorService;
+import com.cms4j.plant.user.service.ExamineeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +17,26 @@ public class WechatAppMajorController extends ApiBaseController {
 
     @Autowired
     private HasmajorService hasmajorService;
+    @Autowired
+    private MjscoreService mjscoreService;
+    @Autowired
+    private ExamineeService examineeService;
+
     @RequestMapping(value = "/get")
     public InvokeResult get() throws Exception {
         DataMap dataMap = this.getDataMap();
 
         return InvokeResult.success(hasmajorService.getHasmajorByScId(dataMap));
+    }
+
+    @RequestMapping(value = "/getmajorbyschool")
+    public InvokeResult getMajorBySchool() throws Exception {
+        DataMap dataMap = this.getDataMap();
+        DataMap curUser = SessionUtil.getCurUser();
+        DataMap examinee = examineeService.getExamineeByUserId(curUser);
+        if(examinee == null) return InvokeResult.failure("请完善考生信息");
+        dataMap.put("MAJORTYPE_ID", examinee.getString("MAJORTYPE"));
+
+        return InvokeResult.success(mjscoreService.getMjscoreBySchoolAndMajor(dataMap));
     }
 }

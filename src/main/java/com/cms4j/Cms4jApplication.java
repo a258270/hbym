70e352dd61d5de4460d245c5b81f7e8d436e7c49
ls <xiaoheima121@163.com>
2017-12-19 16:26:59 +0800

    ls:删除多余一条导包信息

--- src/main/java/com/cms4j/Cms4jApplication.java
+++ src/main/java/com/cms4j/Cms4jApplication.java
@@ -11,7 +11,6 @@ import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomi
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.web.servlet.ErrorPage;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;

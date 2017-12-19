7cfb9df33229a29f0af0e1c7f4737a51df578152
ls <xiaoheima121@163.com>
2017-12-19 17:24:38 +0800

    ls:小程序专业库

--- src/main/java/com/cms4j/plant/major/controller/MajorPageApiController.java
+++ src/main/java/com/cms4j/plant/major/controller/MajorPageApiController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping(value = "/plant/major/api")
+@RequestMapping(value = {"/plant/major/api", "/wechat/applet/major"})
 public class MajorPageApiController extends ApiBaseController {
 
     @Autowired
